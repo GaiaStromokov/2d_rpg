9c0bdac5c95015802ef0e5a31fc93b581a9a812d
GaiaStromokov <gaiamitchener@gmail.com>
2025-01-04 00:13:41 -0800

    changed enemy_class

--- Enemy_class.py
+++ Enemy_class.py
@@ -3,42 +3,42 @@ os.system('cls' if os.name == 'nt' else 'clear')
 def roll(sides): return random.randint(1, sides)
 
 
-nature_dict = Box({
+n_dict = Box({
     "Weak": {
-        "Bod": "E",
-        "Mnd": "E",
-        "Hrt": "E",
-        "Agi": "E",
-        "Str": "E",
-        "Dex": "E",
-        "Int": "E",
-        "Poi": "E",
-        "Dod": "E",
-        "Wer": "E"
+        "Bod": .75,
+        "Mnd": .75,
+        "Hrt": .75,
+        "Agi": .75,
+        "Str": .75,
+        "Dex": .75,
+        "Int": .75,
+        "Poi": .75,
+        "Dod": .75,
+        "Wer": .75
     },
     "Normal": {
-        "Bod": "C",
-        "Mnd": "C",
-        "Hrt": "C",
-        "Agi": "C",
-        "Str": "C",
-        "Dex": "C",
-        "Int": "C",
-        "Poi": "C",
-        "Dod": "C",
-        "Wer": "C"
+        "Bod": 1,
+        "Mnd": 1,
+        "Hrt": 1,
+        "Agi": 1,
+        "Str": 1,
+        "Dex": 1,
+        "Int": 1,
+        "Poi": 1,
+        "Dod": 1,
+        "Wer": 1
     },
     "Strong": {
-        "Bod": "A",
-        "Mnd": "A",
-        "Hrt": "A",
-        "Agi": "A",
-        "Str": "A",
-        "Dex": "A",
-        "Int": "A",
-        "Poi": "A",
-        "Dod": "A",
-        "Wer": "A"
+        "Bod": 1.25,
+        "Mnd": 1.25,
+        "Hrt": 1.25,
+        "Agi": 1.25,
+        "Str": 1.25,
+        "Dex": 1.25,
+        "Int": 1.25,
+        "Poi": 1.25,
+        "Dod": 1.25,
+        "Wer": 1.25
     },
 })
 m_dict = Box({
@@ -87,18 +87,33 @@ class Enemy:
     def __init__(self, Name, cr):
         self.Name = Name
         self.Cr = cr
-        self.Nature = list(nature_dict.keys())[roll(len(nature_dict)) - 1]
-        self.Stats = Box({stat: {"B": (base := m_dict[Name].Stats[stat] + nature_dict[self.Nature][stat]), "V": base} for stat in m_dict[Name].Stats})
-
+        self.Nature = list(n_dict.keys())[roll(len(n_dict)) - 1]
+        self.Stats = Box({stat: {"B": (base := m_dict[Name].Stats[stat] * n_dict[self.Nature][stat]), "V": base} for stat in m_dict[Name].Stats})
+        self.Stats.Hrt.B = round(self.Stats.Hrt.B + self.c_stat(self.Stats.Bod.B) + self.c_stat(self.Stats.Mnd.B), 2)
+        self.Stats.Agi.B = round(self.Stats.Agi.B + self.c_stat(self.Stats.Bod.B) + self.c_stat(self.Stats.Mnd.B), 2)
+        self.Stats.Str.B = round(self.Stats.Str.B + self.c_stat(self.Stats.Bod.B) + self.c_stat(self.Stats.Mnd.B), 2)
+        self.Stats.Dex.B = round(self.Stats.Dex.B + self.c_stat(self.Stats.Agi.B), 2)
+        self.Stats.Int.B = round(self.Stats.Int.B + self.c_stat(self.Stats.Mnd.B), 2)
+        self.Stats.Poi.B = round(self.Stats.Poi.B + self.c_stat(self.Stats.Hrt.B) + self.c_stat(self.Stats.Mnd.B) + self.c_stat(self.Stats.Bod.B), 2)
+        self.Stats.Dod.B = round(self.Stats.Dod.B + self.c_stat(self.Stats.Agi.B) + self.c_stat(self.Stats.Dex.B), 2)
+        self.Stats.Wer.B = round(self.Stats.Wer.B + self.c_stat(self.Stats.Int.B)+ self.c_stat(self.Stats.Mnd.B), 2)
+        self.reset_stats()
+    
     def config_stats(self):
-        self.Stats.Hrt.B = self.Stats.Hrt.B + self.Stats.Bod.B 
-        self.Stats.Agi.B = self.Stats.Agi.B      
-        self.Stats.Str.B = self.Stats.Str.B      
-        self.Stats.Dex.B = self.Stats.Dex.B      
-        self.Stats.Int.B = self.Stats.Int.B      
-        self.Stats.Pos.B = self.Stats.Pos.B      
-        self.Stats.Dod.B = self.Stats.Dod.B      
-        self.Stats.Wer.B = self.Stats.Wer.B      
+        self.Stats.Hrt.V = round(self.Stats.Hrt.V + self.c_stat(self.Stats.Bod.V) + self.c_stat(self.Stats.Mnd.V), 2)
+        self.Stats.Agi.V = round(self.Stats.Agi.V + self.c_stat(self.Stats.Bod.V) + self.c_stat(self.Stats.Mnd.V), 2)
+        self.Stats.Str.V = round(self.Stats.Str.V + self.c_stat(self.Stats.Bod.V) + self.c_stat(self.Stats.Mnd.V), 2)
+        self.Stats.Dex.V = round(self.Stats.Dex.V + self.c_stat(self.Stats.Agi.V), 2)
+        self.Stats.Int.V = round(self.Stats.Int.V + self.c_stat(self.Stats.Mnd.V), 2)
+        self.Stats.Poi.V = round(self.Stats.Poi.V + self.c_stat(self.Stats.Hrt.V) + self.c_stat(self.Stats.Mnd.V) + self.c_stat(self.Stats.Bod.V), 2)
+        self.Stats.Dod.V = round(self.Stats.Dod.V + self.c_stat(self.Stats.Agi.V) + self.c_stat(self.Stats.Dex.V), 2)
+        self.Stats.Wer.V = round(self.Stats.Wer.V + self.c_stat(self.Stats.Int.V)+ self.c_stat(self.Stats.Mnd.V), 2)
+    def c_stat(self, stat):
+        return 0.011*(stat**2)
+    def reset_stats(self):
+        for stat in self.Stats:
+            self.Stats[stat].V = self.Stats[stat].B
+        
     def __str__(self):
         stats_str = "\n".join(f"  {stat}: B = {self.Stats[stat]['B']}, V = {self.Stats[stat]['V']}"for stat in self.Stats)
         return (
